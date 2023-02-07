0bd74b1ccfa576bb1e41620c1ada418b7622abe8
Masum(221-15-5571) <123655995+masumbillah06@users.noreply.github.com>
2023-02-08 03:15:39 +0600

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,21 @@
+public class Method {
+
+    static int met(int x){
+
+        System.out.println("Hello, World");
+        return x+10;
+    }
+    public double methode(int y){
+        System.out.println("Hello, World");
+        return y+20;
+    }
+
+    public static void main(String[] args) {
+
+        int a = 5,j,k;
+
+        System.out.println(j = met(a));
+        Method x = new Method();
+        System.out.println(x.methode(a));
+    }
+}
